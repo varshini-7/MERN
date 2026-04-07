e290c1a045715db84fbc1fceccc807f597a7f39d
varshini-7 <m.varshini077@gmail.com>
2026-04-07 11:39:50 +0530

    Create mongodb_read.js

---
+++ week8/week8/week8/mongodb_read.js
@@ -0,0 +1,14 @@
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/mernlab');
+
+const Student = mongoose.model("Student",{
+name:String,
+age:Number
+});
+
+Student.find()
+.then(data=>{
+console.log(data);
+});
